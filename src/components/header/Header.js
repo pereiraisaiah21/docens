07135c656e1b3bbec0a29ed18e130f56fad9886a
pereiraisaiah21 <95185208+pereiraisaiah21@users.noreply.github.com>
2022-09-04 20:41:17 -0300

    Ajustes links menu temp

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -45,52 +45,52 @@ function Header () {
 			</div>
         </header>
 		<div className="hdr__wrppr view">
-				<a href="/cursos" title="" className="">
+				<a href="https://docens.herokuapp.com/cursos" title="" className="">
 					Cursos
 				</a>
-				<a href="/cursos/10" title="" className="">
+				<a href="https://docens.herokuapp.com/cursos/10" title="" className="">
 					Curso
 				</a>
-				<a href="/materias" title="" className="">
+				<a href="https://docens.herokuapp.com/materias" title="" className="">
 					Materias
 				</a>
-				<a href="/materias/10" title="" className="">
+				<a href="https://docens.herokuapp.com/materias/10" title="" className="">
 					Materia
 				</a>
-				<a href="/materias/posts/1" title="" className="">
+				<a href="https://docens.herokuapp.com/materias/posts/1" title="" className="">
 					Artigo
 				</a>
-				<a href="/quiz/posts/1" title="" className="">
+				<a href="https://docens.herokuapp.com/quiz/posts/1" title="" className="">
 					Quiz
 				</a>
-				<a href="/feed" title="" className="">
+				<a href="https://docens.herokuapp.com/feed" title="" className="">
 					Feed
 				</a>
-				<a href="/perfil/usuario" title="" className="">
+				<a href="https://docens.herokuapp.com/perfil/usuario" title="" className="">
 					Perfil
 				</a>
-				<a href="/entrar" title="" className="">
+				<a href="https://docens.herokuapp.com/entrar" title="" className="">
 					Login
 				</a>
-				<a href="/mensagens" title="" className="">
+				<a href="https://docens.herokuapp.com/mensagens" title="" className="">
 					Alerta
 				</a>
-				<a href="/ajuda" title="" className="">
+				<a href="https://docens.herokuapp.com/ajuda" title="" className="">
 					Ajuda
 				</a>
-				<a href="/contato" title="" className="">
+				<a href="https://docens.herokuapp.com/contato" title="" className="">
 					Contato
 				</a>
-				<a href="cursos/alt/0" title="" className="">
+				<a href="https://docens.herokuapp.com/cursos/alt/0" title="" className="">
 					Cadastrar curso
 				</a>
-				<a href="http://localhost:3000/materias/alt" title="" className="">
+				<a href="https://docens.herokuapp.com/materias/alt" title="" className="">
 					Cadastrar matéria
 				</a>
-				<a href="http://localhost:3000/materias/alt" title="" className="">
+				<a href="https://docens.herokuapp.com/materias/alt" title="" className="">
 					Cadastrar artigo
 				</a>
-				<a href="http://localhost:3000/quiz/alt/posts/1" title="" className="">
+				<a href="https://docens.herokuapp.com/quiz/alt/posts/1" title="" className="">
 					Cadastrar pergunta
 				</a>
 			</div>
