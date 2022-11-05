3aecdcba80f62d6e2a27f6f2309f951967eeadc9
pereiraisaiah21 <95185208+pereiraisaiah21@users.noreply.github.com>
2022-11-05 00:47:05 -0300

    feat: moderate page componetns

--- src/pages/moderation/ArticlesList.js
+++ src/pages/moderation/ArticlesList.js
@@ -18,10 +18,48 @@ function ArticlesList () {
             {
                 url: "/fadsfs",
                 name: "Algebra 01",
+                author: "Rosemberg",
+                authorIcon: "",
                 image: "",
                 imageAlt: "Imagem da matéria de algebra",
-                description: "Descripção de algebra"
-            }
+                approved : false
+            },
+            {
+                url: "/fadsfs",
+                name: "Algebra 01",
+                author: "Rosemberg",
+                authorIcon: "",
+                image: "",
+                imageAlt: "Imagem da matéria de algebra",
+                approved : false
+            },
+            {
+                url: "/fadsfs",
+                name: "Algebra 01",
+                author: "Rosemberg",
+                authorIcon: "",
+                image: "",
+                imageAlt: "Imagem da matéria de algebra",
+                approved : true
+            },
+            {
+                url: "/fadsfs",
+                name: "Algebra 01",
+                author: "Rosemberg",
+                authorIcon: "",
+                image: "",
+                imageAlt: "Imagem da matéria de algebra",
+                approved : false
+            },
+            {
+                url: "/fadsfs",
+                name: "Algebra 01",
+                author: "Rosemberg",
+                authorIcon: "",
+                image: "",
+                imageAlt: "Imagem da matéria de algebra",
+                approved : true
+            },
         ]
     })
 
@@ -33,16 +71,23 @@ function ArticlesList () {
     return (
 
         <section className="articlesList">
-            <MainTitle description="sobre nós" descriptionUnder="Docens Educacional" />
+            <MainTitle description="itens a serem aprovados" descriptionUnder="" />
             <main className="articlesList__wrapper">
                 {
                     articles.data 
                     ?
                     articles.data.map( ( item, key ) => {
                         return (
-                            <div>
-                                <a>
-                                    <span> { item.name } </span>
+                            <div className="articlesList__item">
+                                <a className="articlesList__anchor">
+                                    <span className="articlesList__name"> { item.name } </span>
+
+                                    <div className="articlesList__author">
+                                        <img alt="" className="articlesList__image" src="https://via.placeholder.com/30" />
+                                        <span className="articlesList__text">
+                                            {item.author}
+                                        </span>
+                                    </div>
                                 </a>
                             </div>
                         )
@@ -51,6 +96,25 @@ function ArticlesList () {
                     null
                 }
             </main>
+            <MainTitle description="artigos moderados" descriptionUnder="" />
+            <div className="articlesList__wrapper">
+                {
+                    articles.data
+                    ?
+                    articles.data.map( ( item, key ) => {
+                        return (
+                            <div className="articlesList__item">
+                                <a className="articlesList__anchor">
+                                    <span className="articlesList__name"> { item.name } </span>
+                                    <img className="articlesList__approved" src={`/${item.approved}.svg`} title="" />
+                                </a>
+                            </div>
+                        )
+                    })
+                    :
+                    null
+                }
+            </div>
         </section>
     );
 }
