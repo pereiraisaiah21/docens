a36ca2d181add7c422912d385ab79f8ab05092a3
pereiraisaiah21 <pereiraisaiah21@gmail.com>
2022-11-21 14:39:27 -0300

    fix: article page

--- src/pages/matter/Article.js
+++ src/pages/matter/Article.js
@@ -9,6 +9,9 @@ import MatterTags from "./components/MatterTags";
 import { TailSpin } from "react-loader-spinner";
 import { FaCalendarWeek } from 'react-icons/fa';
 import ConnectionTimeoutWarn from "../../components/alert/ConnectionTimeoutWarn";
+import Emoji from 'a11y-react-emoji';
+import MainTitle from "../../components/title/MainTitle";
+
 
 /**
  *
@@ -20,16 +23,19 @@ function Article () {
     const {id} = useParams();
     const {contentid} = useParams();
 
-    const textTestP = "<p>Às vezes, problemas que parecem muito ?</p><p><img class='img' src='https://img.freepik.com/vetores-premium/formulas-de-fisica-equacoes-matematicas-calculos-aritmeticos-blackboard-com-formulas-cientificas_461812-424.jpg' alt=''/><p class='wrnPrgrph'>Aqui está relacionado ao rei William, que é a melhor rota entre Dallas no Texas</p>";
+    // const textTestP = "<p>Às vezes, problemas que parecem muito ?</p><p><img class='img' src='https://img.freepik.com/vetores-premium/formulas-de-fisica-equacoes-matematicas-calculos-aritmeticos-blackboard-com-formulas-cientificas_461812-424.jpg' alt=''/><p class='wrnPrgrph'>Aqui está relacionado ao rei William, que é a melhor rota entre Dallas no Texas</p>";
+    const textTestP = "";
     const [ loader, setLoader ] = useState( true );
     const [ articleContent, setArticleContent ] = useState({
         data: [],
         error: ""
     });
-    const [ articleRelated, setArticleRelated ] = useState({
-        data: [],
-        error: ""
-    });
+    const icon = ["🛄", "🛄", "🗿", "⚱", "🪣", "🧹", "🧷", "🧴", "🪑", "🛋", "🪟", - "🩺", "🩹", "🔬", "🧬", "🧰", "🪜", "⛓", "📉", "⚙", "📦", "🖊", "🪙"];
+
+    // const [ articleRelated, setArticleRelated ] = useState({
+    //     data: [],
+    //     error: ""
+    // });
     // const [ articleTag, setArticleTag ] = useState({
     //     data: [],
     //     error: ""
@@ -60,30 +66,30 @@ function Article () {
                 // setFailToLoad( true );
             });
 
-        api
-            .get( `https://jsonplaceholder.typicode.com/users/3` )
-            .then( response => {
+        // api
+        //     .get( `https://jsonplaceholder.typicode.com/users/3` )
+        //     .then( response => {
 
-                console.log(response.data)
-                setArticleRelated({
-                    ...articleRelated,
-                    data: [response.data]
-                });
-            }).catch( err => {
-                setArticleRelated({
-                    ...articleRelated,
-                    error: err
-                });
-            });
+        //         console.log(response.data)
+        //         setArticleRelated({
+        //             ...articleRelated,
+        //             data: [response.data]
+        //         });
+        //     }).catch( err => {
+        //         setArticleRelated({
+        //             ...articleRelated,
+        //             error: err
+        //         });
+        //     });
     }, []);
 
     const dataPlaceholder = [
     {
         name : "Algebra",
-        subTitle: "Alg 2",
+        subTitle: "Você sabe oque é lógica? Leia o artigo a seguir e confira mais sobre, e confira também exemplos a seguir",
         quickDescription: "Lorem Ipsum",
         matter: "Matéria X",
-        scope : "Desc",
+        scope : "<p><b>A palavra lógica é originária do grego logos, que significa linguagem racional. É por meio do raciocínio lógico que o homem constrói algoritmos que podem ser transformados em programas capazes de solucionar problemas cada vez mais complexos.</b></p><p>A lógica de programação é necessária para pessoas que desejam trabalhar com desenvolvimento de sistemas e programas, pois ela permite definir a sequência lógica para o desenvolvimento. </p><p>A sequência lógica são passos executados até atingir um objetivo ou solução de um problema.Um algoritmo é formalmente uma sequência finita de passos que levam a execução de uma tarefa. Podemos pensar em algoritmo como uma receita, uma sequência de instruções que dão cabo de uma meta específica. Estas tarefas não podem ser redundantes nem subjetivas na sua definição, devem ser claras e precisas.</p><p>Como exemplos de algoritmos podemos citar os algoritmos das operações básicas (adição, multiplicação, divisão e subtração) de números reais decimais.</p><p>Outros exemplos seriam os manuais de aparelhos eletrônicos, como um videocassete, que explicam passo-a-passo como, por exemplo, gravar um evento.</p><p>Até mesmo as coisas mais simples, podem ser descritas por sequências lógicas. <p>Por exemplo:</p> <p>• Chupar uma bala </p><p>• Pegar a bala </p><p>• Retirar o papel </p><p>• Chupar a bala </p><p>• Jogar o papel no lixo</p>",
         image: "/",
         altImage: "",
         info: 
@@ -96,11 +102,15 @@ function Article () {
             {
                 name: "A mecânica clássica",
                 author: "Isaac Newton"
-            }
+            },
+            {
+                name: "https://unicode.org/emoji/charts/full-emoji-list.html",
+                author: "Isaac Newton"
+            },
         ],
         observer: ["Aqui está relacionado ao rei William, que é a melhor rota entre Dallas no Texas"],
         tags: [
-            "A", "B", "C", "D"
+            "Tag 01", "Tag 02", "Tag 03", "Tag 04"
         ],
         nextMatter:
         {
@@ -141,7 +151,10 @@ function Article () {
                                             </p>
                                             <ImageWithCredits classStyleImg="mttr__img" classStyleCredit="mttr__img__crdts" imageSrc="https://council.science/wp-content/uploads/2017/04/IUPAC-feature-image-1400x600.jpg" iamgeAlt="Descriptions" imageCredits="https://br.freepik.com/vetores-premium/" />
                                             <h4 className="mttr__prmryTtl">
-                                                {item.name}
+                                                <div className="emoji--title">
+                                                    <Emoji className="emoji--navigation" symbol={icon[Math.floor(Math.random() * icon.length)]} label="love" />
+                                                    <MainTitle description={item.name}descriptionUnder="" isCarousel={false} />
+                                                </div>
                                             </h4>
                                             <p className="mttr__scndtyTtl">
                                                 {item.subTitle}
@@ -152,8 +165,7 @@ function Article () {
                                             </div>
                                         </div>
                                         <div className="mttr__prmryWrp mttr__prmryWrp--cntnt">
-                                            {item.scope}
-                                            <div dangerouslySetInnerHTML={{__html:textTestP}} />
+                                            <div dangerouslySetInnerHTML={{__html: item.scope}} />
                                         </div>
                                         {
                                             !!item.userId
@@ -174,7 +186,7 @@ function Article () {
                                         }
                                         <div className="mttr__prmryWrp txt-cntr">
                                             <p className="mttr__spprt">
-                                                Precisa de ajuda? <br /><br /> Confira a seguir alguns conteúdos de apoio para te ajudar.
+                                                Precisa de ajuda? <br /><br /> Confira a seguir alguns conteúdos de recomendação para te ajudar.
                                             </p>
                                             <ol className="mttr__spprt__lst">
                                                 {
@@ -203,10 +215,7 @@ function Article () {
                             ""
                         }
                         </div>
-                        {
-                          console.log('p',articleRelated.data)
-                        }
-                        <RelatedMatters relatedMatter={articleRelated.data}/>
+                        {/* <RelatedMatters relatedMatter={dataPlaceholder[0].nextMatter}/> */}
                         <MatterTags tagMatter={dataPlaceholder[0].tags} />
                         <ButtonWorkout classStyle={"mttr__workout"} url={"/quiz/posts/1"} />
                     </div>
